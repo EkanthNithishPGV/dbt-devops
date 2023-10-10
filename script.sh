a8ab29884facee1229680bc53950f83996b32de3
Ekanth-Nithish <ekanth@squareshift.co>
2023-10-10 17:31:11 +0530

    Cloud run requirements

--- script.sh
+++ script.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
 dbt --version
-dbt --version --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
-dbt deps --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
-
-
 echo dbt run
 dbt run
